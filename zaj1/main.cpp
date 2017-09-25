93615bf470e89fe37ac7a2b775b30fe899995d9c
PiotrTolczyk <32269535+PiotrTolczyk@users.noreply.github.com>
2017-09-25 17:03:19 +0200

    zmiana2

--- zaj1/main.cpp
+++ zaj1/main.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 
 using namespace std;
 
 int main()
 {
-    string imie = "Piotr";
-    string nazwisko = "Tolczyk";
-    int rok_urodzenia = 1997;
-    int aktualny_rok = 2017;
+    string imie;
+    string nazwisko;
+    int rok_urodzenia;
+    int aktualny_rok;
+    cout << "Podaj imie\n";
+    cin >> imie;
+    cout << "\nPodaj nazwisko\n";
+    cin >> nazwisko;
+    cout << "\nPodaj rok urodzenia\n";
+    cin >> rok_urodzenia;
+    cout << "\nPodaj aktualny rok\n";
+    cin >> aktualny_rok;
     cout << "Witam, nazywam sie " << imie << " " << nazwisko << ".\n" << "Mam " << aktualny_rok - rok_urodzenia << " lat.";
+    system "pause";
     return 0;
 }
