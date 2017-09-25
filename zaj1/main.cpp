ea4bb428f25f42c982336de4b1a3d145a01406fa
PiotrTolczyk <32269535+PiotrTolczyk@users.noreply.github.com>
2017-09-25 16:50:40 +0200

    Zmiana

--- zaj1/main.cpp
+++ zaj1/main.cpp
@@ -9,6 +9,6 @@ int main()
     string nazwisko = "Tolczyk";
     int rok_urodzenia = 1997;
     int aktualny_rok = 2017;
-    cout << "Witam, nazywam sie " << imie << " " << nazwisko << ".\n" << "Mam" << aktualny_rok - rok_urodzenia << "lat.";
+    cout << "Witam, nazywam sie " << imie << " " << nazwisko << ".\n" << "Mam " << aktualny_rok - rok_urodzenia << " lat.";
     return 0;
 }
